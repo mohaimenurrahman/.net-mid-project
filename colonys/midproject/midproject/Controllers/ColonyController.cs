50fd57c59cef4653911eeb2795e46308a6a7dfd2
mohaimenurrahman <mohaimenur888@gmail.com>
2022-03-07 01:05:07 +0600

    buildings

--- colonys/midproject/midproject/Controllers/ColonyController.cs
+++ colonys/midproject/midproject/Controllers/ColonyController.cs
@@ -16,5 +16,25 @@ namespace midproject.Controllers
             var data = db.buildings.ToList();
             return View(data);
         }
+
+        [HttpGet]
+        public ActionResult AddBuildings()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddBuildings(building c)
+        {
+            if(ModelState.IsValid)
+            {
+                bdatabaseEntities db = new bdatabaseEntities();
+                db.buildings.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("Buildings");
+
+            }
+            return View(c);
+        }
     }
 }
