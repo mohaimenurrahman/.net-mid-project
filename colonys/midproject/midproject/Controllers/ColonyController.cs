1c0aa11b2dabd544249217dea50e44994d7bd3e0
mohaimenurrahman <mohaimenur888@gmail.com>
2022-03-06 23:49:59 +0600

    buildings table

---
+++ colonys/midproject/midproject/Controllers/ColonyController.cs
@@ -0,0 +1,20 @@
+﻿using midproject.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace midproject.Controllers
+{
+    public class ColonyController : Controller
+    {
+        // GET: Colony
+        public ActionResult Buildings()
+        {
+            bdatabaseEntities db = new bdatabaseEntities();
+            var data = db.buildings.ToList();
+            return View(data);
+        }
+    }
+}
