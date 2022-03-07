e28132ef32af30ca00522d8956a1ade324394f01
mohaimenurrahman <mohaimenur888@gmail.com>
2022-03-08 00:21:43 +0600

    edit

--- colonys/midproject/midproject/Controllers/ColonyController.cs
+++ colonys/midproject/midproject/Controllers/ColonyController.cs
@@ -36,5 +36,61 @@ namespace midproject.Controllers
             }
             return View(c);
         }
+
+
+        public ActionResult BuildingDetails()
+        {
+            bdatabaseEntities db = new bdatabaseEntities();
+            var data = db.buildingDetails.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(buildingDetail b)
+        {
+            if (ModelState.IsValid)
+            {
+                bdatabaseEntities db = new bdatabaseEntities();
+                db.buildingDetails.Add(b);
+                db.SaveChanges();
+                return RedirectToAction("BuildingDetails");
+
+            }
+            return View(b);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            bdatabaseEntities db = new bdatabaseEntities();
+            var data = (from b in db.buildingDetails
+                        where b.buildingCode == id
+                        select b).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(buildingDetail new_Edit)
+        {
+            if (ModelState.IsValid)
+            {
+                bdatabaseEntities db = new bdatabaseEntities();
+                var data = (from b in db.buildingDetails
+                            where b.buildingCode == new_Edit.buildingCode
+                            select b).FirstOrDefault();
+                db.Entry(data).CurrentValues.SetValues(new_Edit);
+                db.SaveChanges();
+                return RedirectToAction("BuildingDetails");
+            }
+            return View();
+        }
+
+
     }
 }
